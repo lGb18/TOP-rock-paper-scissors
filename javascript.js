9c8b9050daa2e1f22b102ec908c8286256082f75
lGb18 <levibarroga18@gmail.com>
2025-08-23 22:34:49 +0800

    Add button with Event Listener for each choice

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
-function playGame(){
+// GAME
+function playGame(pick){
     let humanScore = 0;
     let botScore = 0;
-
+    
     function playRound(humanChoice, computerChoice) {
         if (humanChoice == computerChoice){
         console.log('TIE!!!')
@@ -20,28 +21,45 @@ function playGame(){
         }
         console.log(`Human: ${humanScore} BOT: ${botScore}`) 
     }
-    function getHumanChoice(){
-        return prompt('Enter your choice: ');
-        }
+
+    choices = ["rock", "paper", "scissors"]
+    // function getHumanChoice(){
+    //     return prompt('Enter your choice: ');
+    //     }
     function getComputerChoice() {
-            choices = ["rock", "paper", "scissors"]
             randomChoice = Math.floor(Math.random() * choices.length)
             return choices[randomChoice]
         }
-           
-    while (humanScore < 5 || botScore < 5){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
+    
+    // while (humanScore < 5 || botScore < 5){
+    
+    
+    let computerSelection = getComputerChoice();
+    
+    playRound(pick, computerSelection);
 
-        playRound(humanSelection, computerSelection);
-        if (humanScore == 5) {
-            console.log('Champion: HUMAN')
-            return;
-        }
-        if (botScore == 5) {
-            console.log('Champion: BOT')
-            return;
-        }
-    }
+    //     playRound(humanSelection, computerSelection);
+    //     if (humanScore == 5) {
+    //         console.log('Champion: HUMAN')
+    //         return;
+    //     }
+    //     if (botScore == 5) {
+    //         console.log('Champion: BOT')
+    //         return;
+    //     }
+    // }
 }
-playGame();
+let human_Pick = '';
+// Page
+const choice_Button = document.getElementById("parent-button");
+
+choice_Button.addEventListener('click', (e) => {
+    if(e.target.classList.contains('rock-btn')){
+        human_Pick = "rock";
+    }else if(e.target.classList.contains('paper-btn')) {
+        human_Pick = "paper"
+    }else {
+        human_Pick = "scissors";
+    }
+    playGame(human_Pick);
+})
